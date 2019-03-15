b12bbc9478547a06614de8bbac40a54b7ae78f0f
iner Lofgren <iner.lofgren@gmail.com>
2019-03-14 22:38:32 -0700

    implemented 'like' functionality

--- react-insta-clone/src/components/PostContainer/PostContainer.js
+++ react-insta-clone/src/components/PostContainer/PostContainer.js
@@ -1,27 +1,44 @@
-import React from "react";
+import React, { Component } from "react";
 import { FaRegComment, FaRegHeart } from "react-icons/fa/";
 import CommentSection from "../CommentSection/CommentSection";
 import "./PostContainer.css";
 
-export default function PostContainer(props) {
-  const { thumbnailUrl, username, imageUrl, likes } = props.post;
-  return (
-    <div className="postContainer">
-      <div className="postContainerHeader">
-        <img src={thumbnailUrl} alt="thumbnail" />
-        <h2>{username}</h2>
-      </div>
-      <div className="postContainerPost">
-        <img src={imageUrl} alt="postImage" />
-        <div className="postContainerMessages">
-          <div className="postContainerLikes">
-            <FaRegHeart size={25} />
-            <FaRegComment size={25} />
+class PostContainer extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      likes: props.post.likes
+    };
+  }
+
+  handleClickLike = () => {
+    this.setState(prevState => ({ likes: prevState.likes + 1 }));
+  };
+
+  render() {
+    const { thumbnailUrl, username, imageUrl } = this.props.post;
+    const { likes } = this.state;
+    console.log(this.state.likes);
+    return (
+      <div className="postContainer">
+        <div className="postContainerHeader">
+          <img src={thumbnailUrl} alt="thumbnail" />
+          <h2>{username}</h2>
+        </div>
+        <div className="postContainerPost">
+          <img src={imageUrl} alt="postImage" />
+          <div className="postContainerMessages">
+            <div className="postContainerLikes">
+              <FaRegHeart size={25} onClick={this.handleClickLike} />
+              <FaRegComment size={25} />
+            </div>
+            <h2>{likes} Likes</h2>
+            <CommentSection commentList={this.props.post.comments} />
           </div>
-          <h2>{likes} likes</h2>
-          <CommentSection commentList={props.post.comments} />
         </div>
       </div>
-    </div>
-  );
+    );
+  }
 }
+
+export default PostContainer;
